8dfdb3bead54b03d6db259317c5685cd3362d1fd
Markus Penttilä <wox@iki.fi>
2012-11-28 21:26:57 +0200

    Modified README, added start scripts to git

---
+++ run_distort.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./vacuum --css style.css --players 1 --displaywidth 518 --mode distort --density 0.5
---
+++ run_vacuum.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./vacuum --css style.css --players 1 --displaywidth 518 --density 0.5
